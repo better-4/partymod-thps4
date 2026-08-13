0c58624475c0c63085447e8b076609e5cb5542b2
ellie <149747415+1borgy@users.noreply.github.com>
2026-08-12 22:04:41 -0400

    obs overhaul (#2)
    - removed old observation patches
    - new obs system relies on camera switching, all local
    
    Co-authored-by: zedthps <zkaidovassell@gmail.com>

--- src/input.h
+++ src/input.h
@@ -10,4 +10,8 @@ int CFunc_SetSpinKeysControl(CStruct *params);
 int CFunc_SetSpineTransferControl(CStruct *params);
 int CFunc_SetPauseOnUnfocus(CStruct *params);
 
+extern uint8_t local_observing;
+void SnapObsCameraBack(void);
+void ObsInputDisabled(void);
+
 #endif
