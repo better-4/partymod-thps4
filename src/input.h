7e524a788651e3d56f84451eede09e8d3fd01dd8
ellie <149747415+1borgy@users.noreply.github.com>
2026-08-17 15:24:19 -0400

    move input handling code to better4 source tree

--- src/input.h
+++ src/input.h
@@ -1,17 +1,7 @@
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
-// typedef struct CStruct;
-
 int processIntroEvent();
 void patchInput();
-
-int CFunc_SetSpinKeysControl(void *params);
-int CFunc_SetSpineTransferControl(void *params);
-int CFunc_SetPauseOnUnfocus(void *params);
-
-extern uint8_t local_observing;
-void SnapObsCameraBack(void);
-void ObsInputDisabled(void);
 
 #endif
